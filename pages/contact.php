d3e19b1d29b121a9e682e4b381eb18f7626bd490
webnetkz <8gg001@gmail.com>
2019-02-26 23:56:06 +0600

    update mechanics

--- pages/contact.php
+++ pages/contact.php
@@ -1,1 +1,54 @@
-contact
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Услуги Atcon</title>
+
+        <meta charset="UTF-8">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="Atcon">
+        <meta name="keywords" content="Atcon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="../miniLogoWebnet.png" type="image/png">
+        <link rel="stylesheet" href="../public/css/style.css">
+        <link rel="stylesheet" href="../public/css/mobileStyle.css">
+          
+    </head>
+
+    <body>
+
+        <div id="menu">
+            <nav class="menu">
+                <a href="../index.php"><button class="menuButton first">Главная</button></a>
+                <a href="service.php"><button class="menuButton">Услуги</button></a>
+                <a href="contact.php"><button class="menuButton">Контакты</button></a>
+                <a href="archive.php"><button class="menuButton last">Архив</button></a>
+            </nav>
+        </div>
+
+        <main>
+            <h3 class="contact">AO "Алматытелефонстрой"</h3>
+            <p class="contact"><b>РНН</b> 600300000579</p>
+            <p class="contact"><b>БИК</b> 190501719</p>
+            <p class="contact">Свидет. о постановке на учёт по НДС 60305 № 0049321 oт 12.05.2005</p>
+            <p class="contact">Адрес: г. Алматы ул. Немировича-Данченко, 26</p>
+            <p class="contact"><b>Tелефон:</b> +7 727 2478783</p>
+            <p class="contact"><b>Email:</b> info@atcon.kz</p>
+            <h3 class="contact" style="margin-bottom: 20px;">Генеральный директор Васин В.В.</h3>
+
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1727.689937545063!2d76.86861053672473!3d43.25945757763117!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3883698060369a3f%3A0x87281b66e7dc5d2a!2z0YPQu9C40YbQsCDQndC10LzQuNGA0L7QstC40YfQsC3QlNCw0L3Rh9C10L3QutC-IDI2LCDQkNC70LzQsNGC0Ys!5e0!3m2!1sru!2skz!4v1551202275922" width="100%" height="400px" frameborder="0" style="border:0" allowfullscreen>
+            </iframe>
+        </main>
+
+        <footer>
+            <p>©
+                <?php echo date('Y');?>
+                Алматытелефонстрой.<br> Все права защищены.
+            </p>
+        </footer>
+    
+
+        <script src="public/js/main.js"></script>
+    </body>
+</html>
--- pages/service.php
+++ pages/service.php
@@ -1,1 +1,44 @@
-service
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Услуги Atcon</title>
+
+        <meta charset="UTF-8">
+        <meta name="author" content="TOO WebNet">
+        <meta name="description" content="Atcon">
+        <meta name="keywords" content="Atcon">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="robots" content="index, follow">
+
+        <link rel="shortcut icon" href="../miniLogoWebnet.png" type="image/png">
+        <link rel="stylesheet" href="../public/css/style.css">
+        <link rel="stylesheet" href="../public/css/mobileStyle.css">
+          
+    </head>
+
+    <body>
+
+        <div id="menu">
+            <nav class="menu">
+                <a href="../index.php"><button class="menuButton first">Главная</button></a>
+                <a href="service.php"><button class="menuButton">Услуги</button></a>
+                <a href="contact.php"><button class="menuButton">Контакты</button></a>
+                <a href="archive.php"><button class="menuButton last">Архив</button></a>
+            </nav>
+        </div>
+
+        <main>
+            <a href="services_atcon.kz.xls" download class="download">Скачать прайс услуг</a>
+        </main>
+
+        <footer>
+            <p>©
+                <?php echo date('Y');?>
+                Алматытелефонстрой.<br> Все права защищены.
+            </p>
+        </footer>
+    
+
+        <script src="public/js/main.js"></script>
+    </body>
+</html>
